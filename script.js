a6b6fd8b2ec3cd708a0a1eba2a0eee17e81efbf4
Karimov Islom <126048446+Karimovvv@users.noreply.github.com>
2023-09-12 21:16:42 +0900

    should learn more

--- script.js
+++ script.js
@@ -4,21 +4,39 @@ function circleArea(radius){
     }
     const Pi = Math.PI * Math.pow(radius,2)
     const RoundedArea = Math.round( Pi * 100) / 100
-    console.log(RoundedArea + ' + 0.01');
+    console.log(RoundedArea);
 }
 circleArea(68)
 
 
-function addBinary(a,b,num){ 
+function addBinary(a,b){ 
     const number1 = parseInt(a,2)
     const number2 = parseInt(b,2)
     const sum = number1 + number2
      
     const binary = sum.toString(2)
-    
-    if(num = binary){
-        console.log(`${number1} + ${number2} = ${sum} in decimal or ${binary} in binary`);
-    }
-    console.log(binary);
+
+    console.log(`${number1} + ${number2} = ${sum} in decimal or ${binary} in binary`);
 } 
 addBinary(1,1)
+
+function sumTwoSmallestNumber(number){
+   const sortNumbers = number.sort((a,b) => a - b)
+   const sum = sortNumbers[0] + sortNumbers[1]
+  console.log(sum);
+} 
+sumTwoSmallestNumber([10, 343445353, 3453445, 3453545353453])
+
+function twoInteger(num1 , num2) {
+   const sum = num1 + num2
+  if(num1 === num2){
+    console.log(`${num1} the same numbers ${num2}`);
+  }
+  let sum2 = 0;
+  for (let i = num1; i <= num2; i++){
+    sum2 +=i
+  }
+   
+console.log(sum2)
+}
+twoInteger(1,1)
